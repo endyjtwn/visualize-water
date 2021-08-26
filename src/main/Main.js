39738fe30170d64cf190e07c51ad5e0c69a4b2b6
Endy <endy@Endys-MacBook-Air.local>
2021-08-27 00:36:11 +0700

    random update

---
+++ src/main/Main.js
@@ -0,0 +1,22 @@
+
+function Main() {
+  return (
+    <div className="App">
+              <header className="App-header">
+        <p>
+            <code>water body visualization</code>
+        </p>
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default Main;
